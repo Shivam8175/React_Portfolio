4cb304dde9e281bbf4562dccb678620d99ff697e
Shivam Thakre <st548501@gmail.com>
2025-04-15 19:16:16 +0530

    rct

--- react_portfolio/src/components/sections/Projects.jsx
+++ react_portfolio/src/components/sections/Projects.jsx
@@ -10,49 +10,39 @@ const Projects = () => {
       <RevealOnScroll>
         <div className="max-w-5xl mx-auto px-4">
           <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
-            {" "}
             Featured Projects
           </h2>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
             <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
-              <h3 className="text-xl font-bold mb-2"> MakeMyTrip Clone</h3>
+              <h3 className="text-xl font-bold mb-2">Finance Time Machine</h3>
               <p className="text-gray-400 mb-4">
-                the popular travel booking platform MakeMyTrip, designed to
-                provide users with a seamless experience for booking flights,
-                hotels, and holiday packages. Built using modern web
-                technologies like React.js for the frontend.
+                A dynamic financial data visualization tool that lets users
+                "time travel" through historical stock prices, crypto trends,
+                and economic indicators with an interactive timeline.
               </p>
               <div className="flex flex-wrap gap-2 mb-4">
-                {["React", "Firebase", "css"].map((tech, key) => (
+                {["React", "Firebase", "Tailwindcss"].map((tech, key) => (
                   <span
                     key={key}
-                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
-                                    hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all
-                    "
+                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all"
                   >
                     {tech}
                   </span>
                 ))}
               </div>
-
               <div className="flex justify-between items-center">
                 <a
-                  href="#"
+                  href="https://financialtimemachine.netlify.app/"
+                  target="blank"
                   className="text-blue-400 hover:text-blue-300 transition-colors my-4"
                 >
                   View Project →
                 </a>
               </div>
             </div>
-            <div
-              className="
-              glass p-6 rounded-xl border border-white/10 
-              hover:-translate-y-1 hover:border-blue-500/30
-              hover:shadow-[0_4px_20px_rgba(59,130,246,0.1)]
-              transition-all
-            "
-            >
-              <h3 className="text-xl font-bold mb-2">PortFolio</h3>
+
+            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+              <h3 className="text-xl font-bold mb-2">Portfolio</h3>
               <p className="text-gray-400 mb-4">
                 The Portfolio Project is a dynamic and visually appealing web
                 application designed to showcase an individual's skills,
@@ -61,16 +51,10 @@ const Projects = () => {
                 CSS.
               </p>
               <div className="flex flex-wrap gap-2 mb-4">
-                {["React", "Tailwindcss"].map((tech, key) => (
+                {["React", "Tailwind CSS"].map((tech, key) => (
                   <span
                     key={key}
-                    className="
-                      bg-blue-500/10 text-blue-500 py-1 px-3 
-                      rounded-full text-sm
-                      transition
-                      hover:bg-blue-500/20 hover:-translate-y-0.5
-                      hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]
-                    "
+                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:-translate-y-0.5 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition"
                   >
                     {tech}
                   </span>
@@ -78,7 +62,36 @@ const Projects = () => {
               </div>
               <div className="flex justify-between items-center">
                 <a
-                  href="#"
+                  href=" https://shivamthakreportfolio.netlify.app/ "
+                  target="blank"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
+
+            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+              <h3 className="text-xl font-bold mb-2">VoyageSync</h3>
+              <p className="text-gray-400 mb-4">
+                A full-stack web application replicating the core functionality
+                of MakeMyTrip, enabling users to search and book flights,
+                hotels, and holiday packages with a seamless UI/UX.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["React.js", "Tailwindcss", "Firebase"].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all"
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+              <div className="flex justify-between items-center">
+                <a
+                  href="https://voyage-sync.netlify.app/"
+                  target="blank"
                   className="text-blue-400 hover:text-blue-300 transition-colors my-4"
                 >
                   View Project →
